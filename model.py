dd1cad7ba05b971ded44d29e3768c603674a08f2
Anandhu Raveendran <anandhunair7300@gmail.com>
2025-05-03 08:18:27 +0800

    feat: Updated database to handel login/signup, updated category table for add expense and store added expenses.

--- model.py
+++ model.py
@@ -3,12 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-class User(db.Model):
+from flask_login import UserMixin
+
+class User(UserMixin, db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     email = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
+
 
 
 class Category(db.Model):
@@ -32,6 +35,8 @@ class Item(db.Model):
 
     user = db.relationship("User", backref="items")
     category = db.relationship("Category", backref="items")
+
+
 
 
 def preload_categories():
